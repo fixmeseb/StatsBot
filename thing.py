de6131fe729c4fa13ee4fb7050de387c6e05f35c
fixmeseb <43381771+fixmeseb@users.noreply.github.com>
2020-01-05 10:43:52 -0500

    More updates! It actually works now most of the time except when it doesn't.

--- thing.py
+++ thing.py
@@ -22,7 +22,9 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('$setUpServer'):
+        
+                
+    if message.content.startswith('$serverStatCount'):
         wb = Workbook()
         sheet1 = wb.add_sheet('Sheet 1')
         i = 1
@@ -37,42 +39,41 @@ async def on_message(message):
             sheet1.write(0, x, channel.name)
             x+=1
             print("Added #" + channel.name)
-        for i in range(len(memberList)):
-            for j in range(len(channelList)):
-                sheet1.write(i + 1,j + 1,0)
+        
         wb.save("Beep.xls")
-                
-    if message.content.startswith('$serverStatCount'):
         server = message.guild.text_channels
         z = 0
         for channel in server:
-            async for message in channel.history(limit=200):
-                x = 0
-                y = 0
-                z+=1
-                rb = open_workbook("C:\\Users\\Sebastian_Polge\\OneDrive-CaryAcademy\\Documents\\meNewBot\\Verity\\StatsBot\\Beep.xls",formatting_info=True)
-                r_sheet = rb.sheet_by_index(0) # read only copy to introspect the file
-                wb = copy(rb) # a writable copy (I can't read values out of this, only write to it)
-                w_sheet = wb.get_sheet(0) # the sheet to write to within the writable copy     
+            if (channel.name != "starfall-private-space" and channel.name != "robot-game" and channel.name != "ca-nerd-squad"):
+                for member in message.guild.members:
+                    messageCount = 0
+                    x = 1
+                    y = 1
+                    z+=1
+                    print("Author: " + member.name)
+                    print ("Channel: " + message.channel.name)
+                    sheet = wr.sheet_by_index(0) 
+                    for i in range(sheet.ncols):
+                        #print (sheet.cell_value(0, i) + "," + message.channel.name)
+                        if sheet.cell_value(0,i) == channel.name:
+                            print("Channel is found!") 
+                            y = i
+                    for i in range(sheet.nrows):
+                        #print(sheet.cell_value(i, 0) + "," + member.name) 
+                        if sheet.cell_value(i,0) == member.name:
+                            print("Author is found!") 
+                            x = i
+                    print(str(x) + ";" + str(y))
                 
-                print("Author: " + message.author.name)
-                print ("Channel: " + message.channel.name)
-                sheet = wr.sheet_by_index(0) 
-                for i in range(sheet.ncols):
-                    print (sheet.cell_value(0, i) + "," + message.channel.name)
-                    if sheet.cell_value(0,i) == message.channel.name:
-                        print("Channel is found!") 
-                        y = i
-                for i in range(sheet.nrows):
-                    print(sheet.cell_value(i, 0) + "," + message.author.name) 
-                    if sheet.cell_value(i,0) == message.author.name:
-                        print("Author is found!") 
-                        x = i
-                print(str(x) + ";" + str(y))
-                beep = sheet.cell_value(x, y) + 1
-                print("beep value: " + str(sheet.cell_value(x,y)))
-                sheetR.write(x, y, beep)
-                wb.save("Beep.xls")
+                    async for message in channel.history(limit=None):
+                        if (message.author.name == member.name):
+                            messageCount+=1
+                    print("x: " + str(x) + ", y: " + str(y))
+                    sheet1.write(x, y, int(messageCount))
+                    wb.save("Beep.xls")
+                print("Completed first channel")
+
+
 
                 
 
